ac165209840ef8e8217a97b78c2ab13b7da10d8d
An Vo <107334075+truonganvo@users.noreply.github.com>
2022-10-19 14:19:18 +1100

    Initial commit

---
+++ Assets/Script/AINavMesh.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AINavMesh : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    public Transform[] waypoints; //array of waypoint for the AI to go to.
+    int waypointIndex;
+    Vector3 target;
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, target) < 1)
+        {
+            WayPointIndex();
+            UpdateDestination();
+        }
+    }
+
+    private void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        navMeshAgent.SetDestination(target);
+    }
+
+    void WayPointIndex()
+    {
+        waypointIndex++;
+        if(waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0; //if the current waypoint on equal to the assigned amount of waypoint - reset back to zero
+        }
+    }
+}
---
+++ Assets/Script/CameraPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    public Transform cameraPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
